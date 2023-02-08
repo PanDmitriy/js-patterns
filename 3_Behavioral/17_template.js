77aa52cfd13b8a7bd23eb93bc1c41626508ed919
Dmitri Panzhinski <pandimun@gmail.com>
2023-02-08 22:22:10 +0300

    fix

--- 3_Behavioral/17_template.js
+++ 3_Behavioral/17_template.js
@@ -8,7 +8,7 @@ class Employee {
     this.salary = salary;
   }
 
-  responsibilities() {}
+  responsibilities() {} // шаблон
 
   work() {
     return `${this.name} занимается ${this.responsibilities()}`;
